bc56fefaf8a62734988669bcedc10a781dc6f301
franciapareja <francia.pareja@gmail.com>
2023-05-05 08:21:19 -0500

    Proyecto servidor hoteles

---
+++ API.js
@@ -0,0 +1,22 @@
+import express from "express"
+import {rutas} from "./routes/rutas.js"
+import { establecerconexion } from "./dataBase/conexion.js"
+
+export class API{
+    
+    constructor(){
+        this.app = express()  //APP es express
+        this.conectarBD()
+        this.enrutarPeticiones()
+    }
+    despertarServidor(){
+        this.app.listen(3000,()=> console.log("Servidor encendido..."))
+    }
+    enrutarPeticiones(){
+        this.app.use(express.json())  //habilitamos la recepcion de datos desde el body
+        this.app.use('/', rutas) //habilitamos las rutas o endpoints
+    }
+    conectarBD(){
+        establecerconexion()
+    }
+}
